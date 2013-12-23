dd006b67992b982a15308d011445b29bb8b1d76e
Kyle <kburton@relaynetwork.com>
2013-12-23 10:26:20 -0500

    helper for testing stacktrace, formatting

--- test/logback_riemann_appender/core_test.clj
+++ test/logback_riemann_appender/core_test.clj
@@ -1,26 +1,37 @@
 (ns logback-riemann-appender.core-test
   (:require
    [clojure.test :refer :all]
-   [clojure.tools.logging :as log]))
+   [clojure.tools.logging :as log])
+  (:import
+   [com.github.kyleburton.logback RiemannAppender]))
 
 (deftest test-logging
-  (is (= 0 (.get com.github.kyleburton.logback.RiemannAppender/timesCalled)))
+  (is (= 0 (.get RiemannAppender/timesCalled)))
 
   (log/infof "log at info level")
-  (is (= 1 (.get com.github.kyleburton.logback.RiemannAppender/timesCalled)))
+  (is (= 1 (.get RiemannAppender/timesCalled)))
 
   (log/fatalf "log at fatal level")
-  (is (= 2 (.get com.github.kyleburton.logback.RiemannAppender/timesCalled)))
+  (is (= 2 (.get RiemannAppender/timesCalled)))
 
   (let [ex (RuntimeException. "Test Error")]
-   (log/fatalf ex "log with exception: %s" ex))
-  (is (= 3 (.get com.github.kyleburton.logback.RiemannAppender/timesCalled))))
+    (log/fatalf ex "log with exception: %s" ex))
+  (is (= 3 (.get RiemannAppender/timesCalled))))
+
 
 
 (comment
+  (defn testfnkrb []
+    (let [ex (RuntimeException. "Test exception for logging, what stacktrace do we get?")]
+      (log/infof ex "Testing Stacktrace: %s" ex)))
 
 
   (dotimes [ii 20]
     (log/info "test from clojure"))
 
-)
+  (let [ex (RuntimeException. "Test exception for logging, what stacktrace do we get?")]
+    (log/infof ex "Testing Stacktrace: %s" ex))
+
+  (testfnkrb)
+
+  )
