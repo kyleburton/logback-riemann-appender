c87827fad1914c0fae2e2478dac5c913a382cf74
Dante Briones <dante@cochiva.com>
2015-12-09 13:55:35 -0800

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject com.walmartlabs/logback-riemann-appender "0.1.5"
   :description "Logback Appender that sends events to Riemann"
   :url "https://github.com/walmartlabs/logback-riemann-appender"
-  :lein-release         {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
   :javac-options     ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :junit ["test/java"]
   :plugins [[lein-junit "1.1.8"]
-            [lein-release/lein-release "1.0.5"]
             [lein-swank                "1.4.5"]]
   :profiles {:dev {:dependencies [[junit/junit "4.11"]]
                    :resource-paths ["dev-resources"]
@@ -20,4 +18,12 @@
     [com.aphyr/riemann-java-client  "0.2.8"]
     [ch.qos.logback/logback-classic "1.0.13"]
     [org.clojure/tools.logging      "0.2.6"]]
-  :signing {:gpg-key "dante@walmartlabs.com"})
+  :signing {:gpg-key "dante@walmartlabs.com"}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
